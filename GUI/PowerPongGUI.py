1492ae8d6e1e61b06a97cc209b2cbf5f9c488de1
Ekinsley02 <Ek558@nau.edu>
2025-08-14 20:35:26 -0700

    MotorControl20

--- GUI/PowerPongGUI.py
+++ GUI/PowerPongGUI.py
@@ -163,5 +163,5 @@ class PowerPongPageWidget(QWidget):
 
     def _send_zero_position(self):
         # Send command in SimpleFOC Commander format: "R {offset}"
-        current_offset = 0
+        current_offset = 20
         self._write(f"R {current_offset}\n")
