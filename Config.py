9a83ca0012920e127fbf00b3a0ebc5ff32fbd491
Ekinsley02 <Ek558@nau.edu>
2025-09-27 10:19:39 -0700

    Changed Config

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 # This is the configuration file to alter parameters of the program.
 
-BOARDLESS = True # Set to false if board is present
+BOARDLESS = False # Set to false if board is present
 
-DEVICE = "Windows" # Options: Mac, Linux, Windows
+DEVICE = "Linux" # Options: Mac, Linux, Windows
 
 DEV_MODE = False # Set to true to show escape button in main menu, false to hide it
 
